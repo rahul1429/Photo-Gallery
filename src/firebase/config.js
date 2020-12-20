05cd414b2b0eea1406de9496062d15ab65708fda
rahul1429 <48163243+rahul1429@users.noreply.github.com>
2020-12-20 12:40:24 +0530

    Add files via upload

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,1 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
 
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDh3RzrVwMFHOTKjk_ylra9-nRzFu7UOEY",
+    authDomain: "photogaal-5b503.firebaseapp.com",
+    projectId: "photogaal-5b503",
+    storageBucket: "photogaal-5b503.appspot.com",
+    messagingSenderId: "344139531204",
+    appId: "1:344139531204:web:cde5c7cc19783061861a91"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage = firebase.storage()
+  const projectFirestore = firebase.firestore()
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+  export { projectFirestore, projectStorage, timestamp }
