dab60001a827d741260d630175a3c9362892f1c9
rahul1429 <48163243+rahul1429@users.noreply.github.com>
2020-12-20 12:40:47 +0530

    Add files via upload

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,1 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { projectFirestore } from "../firebase/config";
 
+
+export const useFirestore = (collection) => {
+
+    const [docs, setDocs] = useState([])
+    useEffect(() => {
+        const unsub = projectFirestore.collection(collection)
+            .orderBy('createdAt', 'desc')
+            .onSnapshot((snap) => {
+                let documents = []
+                snap.forEach(doc => {
+                    documents.push({...doc.data(), id: doc.id})
+                })
+                setDocs(documents)
+            })
+
+            return () => unsub()
+
+    }, [collection])
+
+    return { docs }
+}
+
+export default useFirestore
